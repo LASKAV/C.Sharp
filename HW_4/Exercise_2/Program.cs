aef5451b0c9863f09536e29f09bd7558006a346d
LASKAV <mksidoops@gmail.com>
2023-03-23 16:13:32 +0200

    don exe 2

---
+++ HW_4/Exercise_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise_2;
+
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+                        Задание 2
+Пользователь вводит словами цифру от 0 до 9.
+Приложение должно перевести слово в цифру.
+Например, если пользователь ввёл five, приложение должно вывести на экран 5.
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> _Dict = new Dictionary<string, int>
+        {
+            ["zero"] = 0,
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+            ["four"] = 4,
+            ["five"] = 5,
+            ["six"] = 6,
+            ["seven"] = 7,
+            ["eight"] = 8,
+            ["nine"] = 9,
+        };
+        // Dictionary словарь key,val 
+        Console.WriteLine("Введите словами цифру от 0 до 9");
+        string num_str = Console.ReadLine();
+        Console.WriteLine(_Dict[num_str]);
+        Console.Read();
+    }
+}
+
---
+++ HW_4/Exercise_3/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
