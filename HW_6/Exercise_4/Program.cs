9aba47cadb4ef21b7dd7f83985a95169346c4c19
LASKAV <mksidoops@gmail.com>
2023-03-29 21:15:57 +0300

    exer 4 done

--- HW_6/Exercise_4/Program.cs
+++ HW_6/Exercise_4/Program.cs
@@ -16,7 +16,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Worker worker_1 = new President();
+        Worker worker_2 = new Security();
+        Worker worker_3 = new Manager();
+        Worker worker_4 = new Engineer();
+        worker_1.Print();
+        worker_2.Print();
+        worker_3.Print();
+        worker_4.Print();
+        Console.Read();
     }
 }
 
+abstract class Worker
+{
+    public abstract void Print();
+}
+class President : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("President");
+    }
+}
+class Security : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Security");
+    }
+}
+class Manager : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Manager");
+    }
+}
+class Engineer : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Engineer");
+    }
+}
---
+++ HW_7/Exercise_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_1;
+
+/*
+                                Задание 1
+Создайте интерфейс ICalc. В нём должно быть два метода:
+■ intLess(intvalueToCompare) — возвращает количество значений меньших, чем valueToCompare;
+■ intGreater(int valueToCompare) — возвращает количество значений больших, чем valueToCompare.
+Класс, созданный ранее в практическом задании Array, должен имплементировать интерфейс ICalc.
+Метод Less — возвращает количество элементов массива меньших, чем valueToCompare.
+Метод Greater — возвращает количество элементов массива больших, чем valueToCompare.
+Напишите код для тестирования полученной функциональности.
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
